f06775590d979c6aa3084acca9dd1e995b239fc3
docarmo013 <gustavosantos2009g@gmail.com>
2024-09-30 15:21:36 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,27 @@
+
+vertodos.addEventListener("click", function() {
+
+    var escondido = document.getElementById("escondido");
+
+    if(escondido.style.display === "none") {
+        escondido.style.display = "block";
+    } 
+    else {
+        escondido.style.display = "block";
+    }
+})
+
+vertodos.addEventListener("click", function() {
+
+    var ultimo = document.getElementById("ultimo");
+
+    if(ultimo.style.display === "none") {
+        ultimo.style.display = "block";
+    } 
+    else {
+        ultimo.style.display = "block";
+    }
+
+})
+
+
